0c19e4ddc22ac6c0cbb350254a88db558ff53ab3
Raquel Ambrozio <ambrozioraquel@gmail.com>
2024-09-26 14:13:50 -0300

    implementação camada de negócio e exceptions

---
+++ src/main/java/me/dio/service/UserService.java
@@ -0,0 +1,11 @@
+package me.dio.service;
+
+import me.dio.domain.model.User;
+import org.springframework.stereotype.Service;
+
+
+public interface UserService {
+
+    User findById(Long id);
+    User create(User userToCreate);
+}
